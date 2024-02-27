52cf37f75e3a76a7817f91d6fa0c9aca9d80791d
André Alves <146479339+Pinotas@users.noreply.github.com>
2024-02-27 18:30:14 +0000

    3# final

--- polls/routes.js
+++ polls/routes.js
@@ -16,6 +16,30 @@ router.post("/", async (req, res) => {
   res.status(201).json(result);
 });
 
+router.put("/:id/vote", async (req, res) => {
+  const { error, value } = schemas.voteSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json(error.details);
+  }
+  const pollId = req.params.id;
+  const poll = await services.getPollById(pollId);
+  if (!poll) {
+    return res.status(404).json({ error: "poll not found" });
+  }
+  const db = await getDB();
+  const updateRes = await db.collection(pollsCollection).updateOne(
+    { _id: poll._id },
+    {
+      $push: { votes: value },
+    }
+  );
+  if (updateRes.modifiedCount === 0) {
+    return res.status(404).json({ error: "No document updated" });
+  }
+
+  res.status(201).json(value);
+});
+
 router.get("/", async (req, res) => {
   const polls = await services.getAllPolls();
   res.status(200).json(polls);
